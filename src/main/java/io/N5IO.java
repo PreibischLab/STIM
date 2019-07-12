fff8a71ebf63f13b55619052f1f9516109d75d38
Stephan Preibisch <stephan.preibisch@gmx.de>
2019-07-12 11:19:35 +0200

    saving bigger blocks

--- src/main/java/io/N5IO.java
+++ src/main/java/io/N5IO.java
@@ -62,7 +62,7 @@ public class N5IO
 
 		// save the values
 		// numGenes x numCoordinates, 1 block for 1 genes
-		N5Utils.save( expr, n5, "/expression", new int[]{ 16, 512 }, compression, exec );
+		N5Utils.save( expr, n5, "/expression", new int[]{ 16, 16384 }, compression, exec );
 
 		System.out.println( "Saving N5 took " + ( System.currentTimeMillis() - time ) + " ms." );
 
