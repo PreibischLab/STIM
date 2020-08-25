bae38c794998c32eb71aa0754c238b2c3ce4697f
Stephan Preibisch <preibischs@janelia.hhmi.org>
2020-08-25 14:57:49 -0400

    updated global opt sift

--- src/main/java/align/GlobalOptSIFT.java
+++ src/main/java/align/GlobalOptSIFT.java
@@ -19,48 +19,60 @@ import mpicbg.models.PointMatch;
 import mpicbg.models.RigidModel2D;
 import mpicbg.models.Tile;
 import mpicbg.models.TileConfiguration;
-import net.imglib2.multithreading.SimpleMultiThreading;
-import net.imglib2.realtransform.AffineGet;
 import net.imglib2.realtransform.AffineTransform2D;
 import net.imglib2.util.Pair;
-import net.imglib2.util.Util;
 import net.imglib2.util.ValuePair;
 
 public class GlobalOptSIFT
 {
-	protected static List< PointMatch > loadMatches( final String path, final int puckA, final int puckB )
+	public static class Matches
 	{
-		final List< PointMatch > inliers = new ArrayList<>();
+		List< PointMatch > inliers = new ArrayList<>();
 
-		final File file = new File( path + "/slide-seq/sift_1_pm", puckA + "-" + puckB + ".pm.txt" );
+		// pairwise transform
+		AffineTransform2D t = new AffineTransform2D();
+
+		int numInliers = 0;
+		int numCandidates = 0;
+		int puckA = -1;
+		int puckB = -1;
+		String puckAName = "";
+		String puckBName = "";
+
+		public double quality()
+		{
+			return numInliers;
+		}
+	}
+
+	protected static Matches loadMatches( final String path, final int puckA, final int puckB )
+	{
+		final Matches matches = new Matches();
+
+		final File file = new File( path + "/slide-seq/sift_2_pm", puckA + "-" + puckB + ".pm.txt" );
 
 		if ( !file.exists() )
 		{
 			System.out.println( "Cannot find file '" + file.getAbsolutePath() + "'. Returning empty list." );
-			return inliers;
+			return matches;
+		}
+		else
+		{
+			System.out.println( "LOADING '" + file.getAbsolutePath() + "' ..." );
 		}
 
 		final BufferedReader in = TextFileAccess.openFileRead( file.getAbsolutePath() );
-
-		/*
-		if ( ( puckA == 0 && puckB == 2 ) || ( puckA == 0 && puckB == 5 ) || ( puckA == 0 && puckB == 7 )|| ( puckA == 0 && puckB == 8 )|| ( puckA == 0 && puckB == 9 )|| ( puckA == 0 && puckB == 10 )|| ( puckA == 0 && puckB == 11 ) ||
-				( puckA == 1 && puckB == 8 ) ||
-				( puckA == 2 && puckB == 4 ) || ( puckA == 2 && puckB == 7 ) || ( puckA == 2 && puckB == 10 )|| ( puckA == 2 && puckB == 11 ) || 
-				( puckA == 4 && puckB == 5 ) || ( puckA == 4 && puckB == 7 ) || ( puckA == 4 && puckB == 11 ) ||
-				( puckA == 5 && puckB == 7 ) || ( puckA == 5 && puckB == 8 ) || ( puckA == 5 && puckB == 9 ) || ( puckA == 5 && puckB == 10 ) || ( puckA == 5 && puckB == 11 ) ||
-				( puckA == 6 && puckB == 7 ) || ( puckA == 6 && puckB == 11 ) || ( puckA == 6 && puckB == 12 ) ||
-				( puckA == 7 && puckB == 9 ) || ( puckA == 7 && puckB == 10 ) ||
-				( puckA == 8 && puckB == 10 ) || ( puckA == 8 && puckB == 12 ) ||  // vs 12 could be moved
-				( puckA == 9 && puckB == 11 ) )
-			if ( Math.abs( puckA - puckB ) > 2 )
-			return inliers;
-		*/
 
 		try
 		{
 			while ( in.ready() )
 			{
-				final String[] line = in.readLine().trim().split( "\t" );
+				final String input = in.readLine().trim();
+
+				if ( input.equals( "DETAILS" ) )
+					break;
+
+				final String[] line = input.split( "\t" );
 				String p1s = line[ 0 ].trim();
 				String p2s = line[ 1 ].trim();
 
@@ -79,11 +91,36 @@ public class GlobalOptSIFT
 				for ( int d = 0; d < p2.length; ++d )
 					p2[ d ] = Double.parseDouble( p2entries[ d ].trim() );
 
-				inliers.add( new PointMatch( new Point( p1 ), new Point( p2 ) ) );
-
-				//System.out.println( Util.printCoordinates( p1 ) );
-				//System.out.println( Util.printCoordinates( p2 ) );
+				matches.inliers.add( new PointMatch( new Point( p1 ), new Point( p2 ) ) );
 			}
+
+			String[] entries = in.readLine().trim().split( "\t" );
+
+			if ( Integer.parseInt( entries[ 0 ] ) != puckA )
+				throw new RuntimeException( "puck number off:" + puckA + "!=" + Integer.parseInt( entries[ 0 ] ) );
+			if ( puckB != Integer.parseInt( entries[ 1 ] ) )
+				throw new RuntimeException( "puck number off:" + puckB + "!=" + Integer.parseInt( entries[ 1 ] ) );
+			
+			matches.numInliers = Integer.parseInt( entries[ 2 ] ); // inliers
+			matches.numCandidates = Integer.parseInt( entries[ 3 ] ); // candidates
+
+			if ( matches.numInliers != matches.inliers.size() )
+				throw new RuntimeException( "inlier number off:" + matches.numInliers + "!=" + matches.inliers.size() );
+
+			final String model = entries[ 4 ].substring( entries[ 4 ].indexOf( "(" ) + 1, entries[ 4 ].indexOf( ")" )  );
+
+			final String[] mXX = model.split( "," );
+			final double[] m = new double[ 6 ];
+
+			for ( int k = 0; k < m.length; ++k )
+				m[ k ] = Double.parseDouble( mXX[ k ].trim() );
+
+			matches.t = new AffineTransform2D();
+			matches.t.set( m );
+
+			entries = in.readLine().trim().split( "\t" );
+			matches.puckAName = entries[ 0 ].trim();
+			matches.puckBName = entries[ 1 ].trim();
 
 			in.close();
 		}
@@ -93,70 +130,7 @@ public class GlobalOptSIFT
 			return null;
 		}
 
-		return inliers;
-	}
-
-	protected static ArrayList< Alignment > loadPairwiseAlignments( final String file )
-	{
-		final BufferedReader in = TextFileAccess.openFileRead( file );
-
-		final ArrayList< Alignment > alignments = new ArrayList<>();
-
-		final int modelEntry = 4;
-
-		try
-		{
-			while( in.ready() )
-			{
-				final Alignment align = new Alignment();
-
-				// 0	1	59	114	2d-affine: (-0.9992259508792166, -0.03933826495316421, 6086.339263900335, 0.03933826495316421, -0.9992259508792166, 6397.2829715976395)
-				final String[] entries = in.readLine().trim().split( "\t" );
-				int puckA = align.i = Integer.parseInt( entries[ 0 ] );
-				int puckB = align.j = Integer.parseInt( entries[ 1 ] );
-				final int inliers = Integer.parseInt( entries[ 2 ] ); // inliers
-				final int candidates = Integer.parseInt( entries[ 3 ] ); // candidates
-				align.quality = inliers;//(double)inliers / (double)candidates;
-
-				final String model = entries[ modelEntry ].substring( entries[ modelEntry ].indexOf( "(" ) + 1, entries[ modelEntry ].indexOf( ")" )  );
-
-				final String[] mXX = model.split( "," );
-				final double[] m = new double[ 6 ];
-
-				for ( int k = 0; k < m.length; ++k )
-					m[ k ] = Double.parseDouble( mXX[ k ].trim() );
-
-				align.t = new AffineTransform2D();
-				align.t.set( m );
-
-				/*
-				if ( ( puckA == 0 && puckB == 2 ) || ( puckA == 0 && puckB == 5 ) || ( puckA == 0 && puckB == 7 )|| ( puckA == 0 && puckB == 8 )|| ( puckA == 0 && puckB == 9 )|| ( puckA == 0 && puckB == 10 )|| ( puckA == 0 && puckB == 11 ) ||
-					( puckA == 1 && puckB == 8 ) ||
-					( puckA == 2 && puckB == 4 ) || ( puckA == 2 && puckB == 7 ) || ( puckA == 2 && puckB == 10 )|| ( puckA == 2 && puckB == 11 ) || 
-					( puckA == 4 && puckB == 5 ) || ( puckA == 4 && puckB == 7 ) || ( puckA == 4 && puckB == 11 ) ||
-					( puckA == 5 && puckB == 7 ) || ( puckA == 5 && puckB == 8 ) || ( puckA == 5 && puckB == 9 ) || ( puckA == 5 && puckB == 10 ) || ( puckA == 5 && puckB == 11 ) ||
-					( puckA == 6 && puckB == 7 ) || ( puckA == 6 && puckB == 11 ) || ( puckA == 6 && puckB == 12 ) ||
-					( puckA == 7 && puckB == 9 ) || ( puckA == 7 && puckB == 10 ) ||
-					( puckA == 8 && puckB == 10 ) || ( puckA == 8 && puckB == 12 ) ||  // vs 12 could be moved
-					( puckA == 9 && puckB == 11 ) )*/
-				if ( Math.abs( puckA - puckB ) > 2 )
-				{
-					align.quality = 0;
-					align.t = new AffineTransform2D();
-				}
-
-				alignments.add( align );
-			}
-
-			in.close();
-		}
-		catch (IOException e)
-		{
-			e.printStackTrace();
-			return null;
-		}
-
-		return alignments;
+		return matches;
 	}
 
 	public static void main( String[] args ) throws IOException
@@ -164,13 +138,6 @@ public class GlobalOptSIFT
 		final String path = Path.getPath();
 
 		final String[] pucks = new String[] { "Puck_180602_20", "Puck_180602_18", "Puck_180602_17", "Puck_180602_16", "Puck_180602_15", "Puck_180531_23", "Puck_180531_22", "Puck_180531_19", "Puck_180531_18", "Puck_180531_17", "Puck_180531_13", "Puck_180528_22", "Puck_180528_20" };
-
-		// load alignments
-		final ArrayList< Alignment > alignments = loadPairwiseAlignments( new File( path + "/slide-seq", "alignments_sift_1" ).getAbsolutePath() );
-
-		for ( final Alignment align : alignments )
-			if ( align.i < pucks.length && align.j < pucks.length )
-				System.out.println( align.i + "-" + align.j + ": " + align.t );
 
 		final ArrayList< STData > puckData = new ArrayList<STData>();
 		for ( final String puck : pucks )
@@ -216,10 +183,9 @@ public class GlobalOptSIFT
 		{
 			for ( int j = i + 1; j < pucks.length; ++j )
 			{
-				final Alignment align = Alignment.getAlignment( alignments, i, j );
+				final Matches matches = loadMatches( path, i, j );
 
-				quality[ i ][ j ] = quality[ j ][ i ] = align.quality;
-				//quality[ i ][ j ] = quality[ j ][ i ] = 1.0 / GlobalOpt.centerOfMassDistance( puckData.get( i ), puckData.get( j ), new AffineTransform2D(), Alignment.getAlignment( alignments, i, j ).t );
+				quality[ i ][ j ] = quality[ j ][ i ] = matches.quality();
 
 				maxQuality = Math.max( maxQuality, quality[ i ][ j ] );
 				minQuality = Math.min( minQuality, quality[ i ][ j ] );
@@ -254,7 +220,7 @@ public class GlobalOptSIFT
 				tileToIndex.putIfAbsent( tileA, i );
 				tileToIndex.putIfAbsent( tileB, j );
 
-				final List< PointMatch > inliers = loadMatches( path, i, j );
+				final List< PointMatch > inliers = matches.inliers;
 				if ( inliers.size() > 0 )
 				{
 					System.out.println( "Connecting " + i + " to " + j + " ... "); 
