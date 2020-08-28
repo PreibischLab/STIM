c6c5c1c1ab21cf3e767a8c8dd5f77f6fd3890c17
Stephan Preibisch <preibischs@janelia.hhmi.org>
2020-08-28 16:38:27 -0400

    clean up ICP code

--- src/main/java/align/ICPAlign.java
+++ src/main/java/align/ICPAlign.java
@@ -5,17 +5,13 @@ import java.util.Collection;
 import java.util.List;
 
 import data.STData;
-import data.STDataStatistics;
 import imglib2.icp.ICP;
 import imglib2.icp.PointMatchIdentification;
 import imglib2.icp.StDataPointMatchIdentification;
-import mpicbg.models.AbstractAffineModel2D;
-import mpicbg.models.AffineModel2D;
 import mpicbg.models.Model;
 import mpicbg.models.PointMatch;
 import net.imglib2.RealLocalizable;
 import net.imglib2.RealPoint;
-import net.imglib2.realtransform.AffineTransform2D;
 import net.imglib2.util.Pair;
 import net.imglib2.util.ValuePair;
 
@@ -37,42 +33,14 @@ public class ICPAlign
 			final double maxDistance,
 			final int maxIterations )
 	{
-		// regularly sample the reference dataset
-		final STDataStatistics stStatsDataA = new STDataStatistics( stdataA );
-		final STDataStatistics stStatsDataB = new STDataStatistics( stdataB );
-
 		final ArrayList< RealPoint > listA = new ArrayList<>(); // reference
 		final ArrayList< RealPoint > listB = new ArrayList<>(); // target
 
 		for ( final RealLocalizable p : stdataA )
 			listA.add( new RealPoint( p ) );
 
-		//System.out.println( "listA (reference): " + listA.size() );
-
-		/*
-		// tmp
-		System.out.println( "listB (target) sampling: " + StDataPointMatchIdentification.sampling );
-		StDataPointMatchIdentification.sampling = 4.0;//Math.min( stStatsDataA.getMedianDistance(), stStatsDataB.getMedianDistance() ) / 2.0;
-		IterableRealInterval< DoubleType > dataB = TransformCoordinates.sample( stdataB.getExprData( genesToUse.get( 0 ) ), StDataPointMatchIdentification.sampling );
-		final RealCursor< DoubleType > c = dataB.localizingCursor();
-
-		while ( c.hasNext() )
-		{
-			c.fwd();
-			listB.add( new RealPoint( c ) );
-		}
-		*/
 		for ( final RealLocalizable p : stdataB )
 			listB.add( new RealPoint( p ) );
-
-		//System.out.println( "listB (target): " + listB.size() );
-
-		/*
-		final double[] m = initialModel.getRowPackedCopy(); //a.m00, a.m01, a.m02, a.m10, a.m11, a.m12
-		final AffineModel2D model = new AffineModel2D();
-		//model.set( m00, m10, m01, m11, m02, m12 );
-		model.set( m[ 0 ], m[ 3 ], m[ 1 ], m[ 4 ], m[ 2 ], m[ 5 ] );
-		*/
 		
 		final M model = initialModel.copy();
 
@@ -109,19 +77,9 @@ public class ICPAlign
 		while ( !converged && ++i < maxIterations );
 
 		if ( icp.getPointMatches() == null )
-		{
 			return null;
-		}
 		else
-		{
-			/*
-			model.toArray( m ); // The order is: m00, m10, m01, m11, m02, m12
-			final AffineTransform2D result = new AffineTransform2D();
-			// a.m00, a.m01, a.m02, a.m10, a.m11, a.m12
-			result.set( m[ 0 ], m[ 2 ], m[ 4 ], m[ 1 ], m[ 3 ], m[ 5 ] );
-			*/
 			return new ValuePair<>( model, new ArrayList<>( PointMatch.flip( icp.getPointMatches() ) ) );
-		}
 	}
 
 }
