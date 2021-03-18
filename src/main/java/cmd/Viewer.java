45033dc082cff2b40b9f7fb44ab2f4c35dfb28f8
Stephan Preibisch <stephan.preibisch@gmx.de>
2021-03-18 05:28:00 -0400

    first command line tool test

---
+++ src/main/java/cmd/Viewer.java
@@ -0,0 +1,49 @@
+package cmd;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+
+import org.janelia.saalfeldlab.n5.N5FSReader;
+
+import gui.STDataAssembly;
+import io.N5IO;
+import picocli.CommandLine;
+import picocli.CommandLine.Option;
+
+public class Viewer implements Callable<Void> {
+	//TODO: fix viewer brightness
+	//TODO: build function, makes fatjar and sh scripts
+
+	@Option(names = {"-i", "--container"}, required = true, description = "N5 container paths, e.g. -i /home/ssq.n5 ...")
+	private String containerPath = null;
+
+	@Option(names = {"-d", "--datasets"}, required = false, description = "comma separated list of datasets, e.g. -d '/Puck_180528_20,Puck_180528_22' (default: open all datasets)")
+	private List<String> datasets = null;
+
+	@Override
+	public Void call() throws Exception {
+
+		final ArrayList< STDataAssembly > data;
+
+		if ( datasets == null || datasets.size() == 0 )
+		{
+			data = N5IO.openAllDatasets( new File( containerPath ), true );
+		}
+		else
+		{
+			data = new ArrayList<>();
+
+			final N5FSReader n5 = N5IO.openN5( new File( containerPath ) );
+			for ( final String dataset : datasets )
+				data.add( N5IO.openDataset( n5, dataset, true ) );
+		}
+
+		return null;
+	}
+
+	public static final void main(final String... args) {
+		CommandLine.call(new Viewer(), args);
+	}
+}
