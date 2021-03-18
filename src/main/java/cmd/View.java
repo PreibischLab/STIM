a3e08d53cd492b1ad5b807cb503a1548d1ade522
Stephan Preibisch <stephan.preibisch@gmx.de>
2021-03-18 06:43:02 -0400

    added contrast range

--- src/main/java/cmd/View.java
+++ src/main/java/cmd/View.java
@@ -2,11 +2,13 @@ package cmd;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.Callable;
 
 import org.janelia.saalfeldlab.n5.N5FSReader;
 
+import gui.RenderThread;
 import gui.STDataAssembly;
 import gui.STDataExplorer;
 import io.N5IO;
@@ -15,13 +17,15 @@ import picocli.CommandLine.Option;
 
 public class View implements Callable<Void> {
 	//TODO: fix viewer brightness
-	//TODO: build function, makes fatjar and sh scripts
 
 	@Option(names = {"-i", "--container"}, required = true, description = "N5 container paths, e.g. -i /home/ssq.n5 ...")
 	private String containerPath = null;
 
 	@Option(names = {"-d", "--datasets"}, required = false, description = "comma separated list of datasets, e.g. -d '/Puck_180528_20,Puck_180528_22' (default: open all datasets)")
 	private List<String> datasets = null;
+
+	@Option(names = {"-c", "--contrast"}, description = "comma separated contrast range for BigDataViewer display, e.g. -c '0,255' (default 0.1,5)" )
+	private String contrastString = null;
 
 	@Override
 	public Void call() throws Exception {
@@ -32,6 +36,23 @@ public class View implements Callable<Void> {
 		{
 			System.out.println( "'" + n5Path.getAbsolutePath() + "' does not exist. Stopping." );
 			return null;
+		}
+
+		if ( contrastString != null && contrastString.length() > 0 )
+		{
+			String[] contrastStrings = contrastString.trim().split( "," );
+
+			if ( contrastStrings.length != 2 )
+			{
+				System.out.println( "contrast string could not parsed " + Arrays.asList( contrastStrings ) + ", ignoring - setting default range (" + RenderThread.min + "," + RenderThread.max + ")" );
+			}
+			else
+			{
+			RenderThread.min = Double.parseDouble( contrastStrings[ 0 ] );
+			RenderThread.max = Double.parseDouble( contrastStrings[ 1 ] );
+
+			System.out.println( "contrast range set to (" + RenderThread.min + "," + RenderThread.max + ")" );
+			}
 		}
 
 		final ArrayList< STDataAssembly > data;
