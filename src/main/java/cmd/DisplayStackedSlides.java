282cae91d1ffc0151eef198e21b89119fca8aaf2
Stephan Preibisch <stephan.preibisch@gmx.de>
2021-04-11 14:37:59 -0400

    added support for multi-color rendering

--- src/main/java/cmd/DisplayStackedSlides.java
+++ src/main/java/cmd/DisplayStackedSlides.java
@@ -1,9 +1,11 @@
 package cmd;
 
+import java.awt.Color;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 import java.util.concurrent.Callable;
 
 import org.janelia.saalfeldlab.n5.N5FSReader;
@@ -22,6 +24,7 @@ import io.N5IO;
 import net.imglib2.Interval;
 import net.imglib2.RealRandomAccessible;
 import net.imglib2.realtransform.AffineTransform2D;
+import net.imglib2.type.numeric.ARGBType;
 import net.imglib2.type.numeric.real.DoubleType;
 import net.imglib2.util.Pair;
 import picocli.CommandLine;
@@ -32,8 +35,8 @@ public class DisplayStackedSlides implements Callable<Void> {
 	@Option(names = {"-i", "--input"}, required = true, description = "input N5 container, e.g. -i /home/ssq.n5")
 	private String input = null;
 
-	@Option(names = {"-g", "--gene"}, required = true, description = "gene to visualize, e.g. -g Calm2")
-	private String gene = null;
+	@Option(names = {"-g", "--genes"}, required = true, description = "comma separated list of one or more gene to visualize, e.g. -g Calm2,Ubb")
+	private String genes = null;
 
 	@Option(names = {"-z", "--zSpacingFactor"}, required = false, description = "define the z-spacing between differnt sections (as a factor of median spacing between sequenced locations), e.g. -z 10.0 (default: 5.0)")
 	private double zSpacingFactor = 5.0;
@@ -122,15 +125,47 @@ public class DisplayStackedSlides implements Callable<Void> {
 			filterFactorys.add( new MedianFilterFactory<>( outofbounds, median ) );
 		}
 
-		final Pair< RealRandomAccessible< DoubleType >, Interval > stack = VisualizeStack.createStack( slides, gene, outofbounds, zSpacingFactor, filterFactorys );
-		final Interval interval = stack.getB();
+		List< String > genesToShow;
 
-		final BdvOptions options = BdvOptions.options().numRenderingThreads( Runtime.getRuntime().availableProcessors() );
-		BdvStackSource< ? > source = BdvFunctions.show( stack.getA(), interval, gene, options );
-		source.setDisplayRange( minI, maxI );
-		source.setDisplayRangeBounds( 0, 200 );
-		source.getBdvHandle().getViewerPanel().setDisplayMode( DisplayMode.SINGLE );
-		source.setCurrent();
+		if ( genes == null || genes.length() == 0 )
+		{
+			System.out.println( "no genes defined. stopping.");
+			return null;
+		}
+		else
+			genesToShow = Arrays.asList( genes.split( "," ) );
+
+		if ( genesToShow.size() == 0 )
+		{
+			System.out.println( "no genes defined. stopping.");
+			return null;
+		}
+
+		BdvStackSource< ? > source = null;
+		Random rnd = new Random( 343 );
+
+		for ( final String gene : genesToShow )
+		{
+			final Pair< RealRandomAccessible< DoubleType >, Interval > stack = VisualizeStack.createStack( slides, gene, outofbounds, zSpacingFactor, filterFactorys );
+			final Interval interval = stack.getB();
+	
+			final BdvOptions options = BdvOptions.options().numRenderingThreads( Runtime.getRuntime().availableProcessors() ).addTo( source );
+			source = BdvFunctions.show( stack.getA(), interval, gene, options );
+			source.setDisplayRange( minI, maxI );
+			source.setDisplayRangeBounds( 0, 200 );
+			source.getBdvHandle().getViewerPanel().setDisplayMode( DisplayMode.FUSED );
+			source.setCurrent();
+
+			if ( genesToShow.size() > 1 )
+			{
+				final float h = rnd.nextFloat();
+				final float s = rnd.nextFloat();
+				final float b = 0.9f + 0.1f * rnd.nextFloat();
+				final Color c = Color.getHSBColor(h, s, b);
+
+				source.setColor( new ARGBType( ARGBType.rgba(c.getRed(), c.getGreen(), c.getBlue(), c.getAlpha())));
+			}
+		}
 
 		return null;
 	}
