354fb3738d49ca0eba63b750831911883d103253
Stephan Preibisch <stephan.preibisch@gmx.de>
2021-04-29 11:47:12 -0400

    ICP pointmatch identification creates a common set of points across all genes and normalizes so the matching works properly

--- src/main/java/imglib2/icp/StDataPointMatchIdentification.java
+++ src/main/java/imglib2/icp/StDataPointMatchIdentification.java
@@ -27,16 +27,36 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 
+import align.AlignTools;
 import align.ICPAlign;
 import data.STData;
+import data.STDataStatistics;
+import filter.Filters;
+import filter.GaussianFilterFactory;
+import filter.GaussianFilterFactory.WeightType;
+import ij.ImageJ;
+import imglib2.ImgLib2Util;
 import mpicbg.models.PointMatch;
+import net.imglib2.IterableRealInterval;
 import net.imglib2.KDTree;
+import net.imglib2.RealCursor;
 import net.imglib2.RealLocalizable;
+import net.imglib2.RealPoint;
+import net.imglib2.RealPointSampleList;
+import net.imglib2.RealRandomAccessible;
+import net.imglib2.converter.Converters;
+import net.imglib2.img.display.imagej.ImageJFunctions;
+import net.imglib2.multithreading.SimpleMultiThreading;
 import net.imglib2.neighborsearch.KNearestNeighborSearchOnKDTree;
 import net.imglib2.neighborsearch.NearestNeighborSearchOnKDTree;
 import net.imglib2.neighborsearch.RadiusNeighborSearchOnKDTree;
+import net.imglib2.realtransform.AffineTransform2D;
+import net.imglib2.realtransform.RealViews;
+import net.imglib2.type.numeric.RealType;
 import net.imglib2.type.numeric.real.DoubleType;
 import net.imglib2.util.RealSum;
+import net.imglib2.view.Views;
+import render.Render;
 import transform.TransformCoordinates;
 
 public class StDataPointMatchIdentification < P extends RealLocalizable > implements PointMatchIdentification< P >
@@ -47,7 +67,9 @@ public class StDataPointMatchIdentification < P extends RealLocalizable > implem
 	final STData stDataReference;
 	final Collection< String > genes;
 
-	final HashMap< String, NearestNeighborSearchOnKDTree< DoubleType > > searchTarget, searchReference;
+	NearestNeighborSearchOnKDTree< DoubleType > searchTarget, searchReference;
+
+	//final HashMap< String, NearestNeighborSearchOnKDTree< DoubleType > > searchTarget, searchReference;
 
 	double distanceThresold;
 
@@ -58,22 +80,110 @@ public class StDataPointMatchIdentification < P extends RealLocalizable > implem
 		this.genes = genes;
 		this.distanceThresold = distanceThreshold;
 
-		this.searchTarget = new HashMap<>();
-		this.searchReference = new HashMap<>();
+		//this.searchTarget = new HashMap<>();
+		//this.searchReference = new HashMap<>();
 
+		final double dist = Math.max( new STDataStatistics( stDataTarget ).getMedianDistance(), new STDataStatistics( stDataReference ).getMedianDistance() );
+
+		IterableRealInterval< DoubleType > sumReference = null;
+		IterableRealInterval< DoubleType > sumTarget = null;
+
+		
+		// TODO: why not integrate all genes into one value right away?
 		for ( final String gene : genes )
 		{
-			searchReference.put( gene, new NearestNeighborSearchOnKDTree<>( new KDTree<>( stDataReference.getExprData( gene ) ) ) );
+			// normalize per gene 0...1 and gaussian blur first
 
-			searchTarget.put( gene, new NearestNeighborSearchOnKDTree<>( new KDTree<>( stDataTarget.getExprData( gene ) ) ) );
+			IterableRealInterval<DoubleType> ref = stDataReference.getExprData( gene );
+			IterableRealInterval<DoubleType> target = stDataTarget.getExprData( gene );
+
+			ref = Filters.filter( ref, new GaussianFilterFactory<>( new DoubleType( 0 ), dist * 4, WeightType.BY_SUM_OF_WEIGHTS ) );
+			target = Filters.filter( target, new GaussianFilterFactory<>( new DoubleType( 0 ), dist * 4, WeightType.BY_SUM_OF_WEIGHTS ) );
+
+			if ( sumReference == null )
+			{
+				sumReference = new RealPointSampleList<>( stDataReference.numDimensions() );
+				sumTarget = new RealPointSampleList<>( stDataTarget.numDimensions() );
+
+				final RealCursor< DoubleType > cR = ref.localizingCursor();
+
+				while ( cR.hasNext() )
+				{
+					final DoubleType value = cR.next();
+					((RealPointSampleList<DoubleType>)sumReference).add( new RealPoint( cR ), value.copy() );
+				}
+
+				final RealCursor< DoubleType > cT = target.localizingCursor();
+
+				while ( cT.hasNext() )
+				{
+					final DoubleType value = cT.next();
+					((RealPointSampleList<DoubleType>)sumTarget).add( new RealPoint( cT ), value.copy() );
+				}
+			}
+			else
+			{
+				final RealCursor< DoubleType > cR = ref.localizingCursor();
+				final RealCursor< DoubleType > cRout = sumReference.localizingCursor();
+
+				while ( cR.hasNext() )
+					cRout.next().add( cR.next() );
+
+				final RealCursor< DoubleType > cT = target.localizingCursor();
+				final RealCursor< DoubleType > cTout = sumTarget.localizingCursor();
+
+				while ( cT.hasNext() )
+					cTout.next().add( cT.next() );
+			}
+
+			//ref = normalize( ref );
+			//target = normalize( target );
+
+			//searchReference.put( gene, new NearestNeighborSearchOnKDTree<>( new KDTree<>( ref ) ) );
+			//searchTarget.put( gene, new NearestNeighborSearchOnKDTree<>( new KDTree<>( target ) ) );
+
 			// regularly sample the reference dataset
 			//searchTarget.put( gene, new NearestNeighborSearchOnKDTree<>( new KDTree<>( TransformCoordinates.sample( stDataTarget.getExprData( gene ), sampling ) ) ) );
 		}
+
+		RealSum sumT = new RealSum();
+		for ( final DoubleType t : sumTarget )
+			sumT.add( t.get() );
+
+		sumTarget = normalize( sumTarget, 0, sumT.getSum() / (double)sumTarget.size() );
+		this.searchTarget = new NearestNeighborSearchOnKDTree<>( new KDTree<>( sumTarget ) );
+
+		RealSum sumR = new RealSum();
+		for ( final DoubleType t : sumReference )
+			sumR.add( t.get() );
+
+		sumReference = normalize( sumReference, 0, sumR.getSum() / (double)sumReference.size() );
+		this.searchReference = new NearestNeighborSearchOnKDTree<>( new KDTree<>( sumReference ) );
+
+		System.out.println( "avg target: " + sumT.getSum() / (double)sumTarget.size() + ", avg ref: " + sumR.getSum() / (double)sumReference.size() + ", maxDist: " + distanceThreshold );
+
+		/*
+		// visualize the sum of both
+		final AffineTransform2D tS = new AffineTransform2D();
+		tS.scale( 0.05 );
+		new ImageJ();
+		final RealRandomAccessible< DoubleType > renderRRA =
+				Render.render( sumTarget, new GaussianFilterFactory<>( new DoubleType( 0 ), 10, WeightType.PARTIAL_BY_SUM_OF_WEIGHTS ) );
+
+		ImageJFunctions.show( Views.interval( RealViews.affine( renderRRA, tS ), ImgLib2Util.transformInterval( stDataTarget.getRenderInterval(), tS ) ), "target" );
+
+		final RealRandomAccessible< DoubleType > renderRRA2 =
+				Render.render( sumReference, new GaussianFilterFactory<>( new DoubleType( 0 ), 10, WeightType.PARTIAL_BY_SUM_OF_WEIGHTS ) );
+
+		ImageJFunctions.show( Views.interval( RealViews.affine( renderRRA2, tS ), ImgLib2Util.transformInterval( stDataReference.getRenderInterval(), tS ) ), "ref" );
+
+		SimpleMultiThreading.threadHaltUnClean();*/
 	}
 
 	public void setDistanceThreshold( final double distanceThreshold ) { this.distanceThresold = distanceThreshold; }
 	public double getDistanceThreshold() { return this.distanceThresold; }
 
+	/*
 	protected double difference( final RealLocalizable target, final RealLocalizable reference )
 	{
 		double diff = 0;
@@ -93,6 +203,17 @@ public class StDataPointMatchIdentification < P extends RealLocalizable > implem
 		referenceSearch.search( reference );
 
 		return Math.abs( targetSearch.getSampler().get().get() - referenceSearch.getSampler().get().get() );
+	}
+	*/
+
+	protected double difference( final RealLocalizable target, final RealLocalizable reference )
+	{
+		searchTarget.search( target );
+		searchReference.search( reference );
+
+		final double diff = Math.abs( searchTarget.getSampler().get().get() - searchReference.getSampler().get().get() );
+
+		return diff;
 	}
 
 	@Override
@@ -117,11 +238,11 @@ public class StDataPointMatchIdentification < P extends RealLocalizable > implem
 			
 			nnSearchTarget.search( referencePoint, distanceThresold, false );
 
-			//double minDiff = Double.MAX_VALUE;
-			//LinkedPoint< P > bestTargetPoint = null;
+			double minDiff = Double.MAX_VALUE;
+			LinkedPoint< P > bestTargetPoint = null;
 			
-			double maxBrightness = -Double.MAX_VALUE;
-			LinkedPoint< P > brightestPoint = null;
+			//double maxBrightness = -Double.MAX_VALUE;
+			//LinkedPoint< P > brightestPoint = null;
 			
 			// find the brightest point in a certain radius (we preselected the reference point to be the brightnest in a certain region)
 			for ( int i = 0; i < nnSearchTarget.numNeighbors(); ++i )
@@ -132,40 +253,60 @@ public class StDataPointMatchIdentification < P extends RealLocalizable > implem
 				// now we need the location of the original point for the transformed target point we found, because we look up the gene expression values there
 				final RealLocalizable targetLocation = targetPoint.getLinkedObject();
 
-				/*
-				final double expDiff = difference( targetLocation, referencePoint );
+				final double expDiff = difference( targetLocation, referencePoint ); // TODO: shouldn't it be referencePoint.getLinkedObject() (or always identity transform?)
 
 				if ( expDiff < minDiff )
 				{
 					minDiff = expDiff;
 					bestTargetPoint = targetPoint;
 				}
-				*/
 
+				/*
 				final double sum = ICPAlign.computeSum( targetLocation, searchTarget );
 				if ( sum > maxBrightness )
 				{
 					maxBrightness = sum;
 					brightestPoint = targetPoint;
 				}
+				*/
 			}
 
-			/*
 			if ( bestTargetPoint != null )
 			{
 				pointMatches.add( new PointMatch( bestTargetPoint, referencePoint ) );
 				sumDiff.add( minDiff );
 				++numMatches;
 			}
-			*/
 
+			/*
 			if ( brightestPoint != null && maxBrightness > 0 )
 				pointMatches.add( new PointMatch( brightestPoint, referencePoint ) );
-
+			*/
 		}
 
-		//System.out.println("Assigned " + numMatches + " with avg error = " + (sumDiff.getSum() / numMatches ) );
+		System.out.println("Assigned " + numMatches + " with avg error = " + (sumDiff.getSum() / numMatches ) );
 
 		return pointMatches;
 	}
+
+	public static < T extends RealType< T > > IterableRealInterval< T > normalize( final IterableRealInterval<T> ref )
+	{
+		double min = Double.MAX_VALUE;
+		double max = -Double.MAX_VALUE;
+
+		for ( final T v : ref )
+		{
+			min = Math.min( min, v.getRealDouble() );
+			max = Math.max( max, v.getRealDouble() );
+		}
+
+		return normalize( ref, min, max );
+	}
+
+	public static < T extends RealType< T > > IterableRealInterval< T > normalize( final IterableRealInterval<T> ref, final double min, final double max )
+	{
+		final double diff = max-min;
+		return Converters.convert( ref, (i,o) -> o.setReal( (i.getRealDouble() - min)/(diff) ), ref.firstElement() );
+	}
+
 }
