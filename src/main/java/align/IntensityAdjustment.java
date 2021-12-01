38c9399117a175bbebef54b8045aff27ea76733b
Stephan Preibisch <stephan.preibisch@gmx.de>
2021-12-01 15:09:50 -0500

    significant changes making intensity adjustment more robust ... not sure it's working too well right now

--- src/main/java/align/IntensityAdjustment.java
+++ src/main/java/align/IntensityAdjustment.java
@@ -3,23 +3,38 @@ package align;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map.Entry;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
 import java.util.Random;
 
 import org.janelia.saalfeldlab.n5.N5FSReader;
 import org.janelia.saalfeldlab.n5.N5FSWriter;
+import org.netlib.util.doubleW;
 
 import align.GlobalOptSIFT.Matches;
+import data.NormalizingSTData;
 import data.STData;
 import data.STDataStatistics;
+import filter.Filters;
+import filter.GaussianFilterFactory;
+import filter.GaussianFilterFactory.WeightType;
+import ij.ImageJ;
 import io.N5IO;
 import io.Path;
 import mpicbg.models.Affine1D;
 import mpicbg.models.AffineModel1D;
+import mpicbg.models.AffineModel2D;
+import mpicbg.models.IdentityModel;
 import mpicbg.models.IllDefinedDataPointsException;
 import mpicbg.models.InterpolatedAffineModel1D;
 import mpicbg.models.Model;
@@ -28,16 +43,24 @@ import mpicbg.models.Point;
 import mpicbg.models.PointMatch;
 import mpicbg.models.Tile;
 import mpicbg.models.TileConfiguration;
+import mpicbg.models.TileUtil;
 import mpicbg.models.TranslationModel1D;
 import net.imglib2.IterableRealInterval;
 import net.imglib2.KDTree;
 import net.imglib2.RealCursor;
+import net.imglib2.img.display.imagej.ImageJFunctions;
+import net.imglib2.multithreading.SimpleMultiThreading;
 import net.imglib2.neighborsearch.NearestNeighborSearchOnKDTree;
+import net.imglib2.neighborsearch.RadiusNeighborSearch;
+import net.imglib2.neighborsearch.RadiusNeighborSearchOnKDTree;
 import net.imglib2.realtransform.AffineTransform2D;
 import net.imglib2.type.numeric.real.DoubleType;
 import net.imglib2.util.Pair;
 import net.imglib2.util.Util;
 import net.imglib2.util.ValuePair;
+import render.Render;
+import util.Threads;
+import weka.Run;
 
 public class IntensityAdjustment
 {
@@ -47,17 +70,16 @@ public class IntensityAdjustment
 			final ArrayList< STData > puckData,
 			final ArrayList< AffineTransform2D > transforms,
 			final double maxDistance,
-			final int maxMatches )
+			final int maxMatches,
+			final boolean correctScaling,
+			final int nThreads )
 	{
-		final HashMap< Pair< Integer, Integer >, ArrayList< PointMatch > > intensityMatches = new HashMap<>();
+		final HashSet< String > genes = new HashSet<>();
 
 		for ( int i = 0; i < pucks.size() - 1; ++i )
 		{
 			for ( int j = i + 1; j < pucks.size(); ++j )
 			{
-				// corresponding intensity values
-				final ArrayList< PointMatch > localMatches = new ArrayList<>();
-
 				// load the matches
 				final Matches siftmatches = GlobalOptSIFT.loadMatches( n5, pucks.get( i ), pucks.get( j ) );
 
@@ -67,72 +89,209 @@ public class IntensityAdjustment
 				// use all genes that were used for alignment
 				for ( final String gene : siftmatches.genes )
 				{
-					// create two transformed IterableRealIntervals
-					final IterableRealInterval< DoubleType > dataI = puckData.get( i ).getExprData( gene );
-					final IterableRealInterval< DoubleType > dataJ = puckData.get( j ).getExprData( gene );
-
-					final KDTree< DoubleType > treeJ = new KDTree<>( dataJ );
-					final NearestNeighborSearchOnKDTree< DoubleType > searchJ = new NearestNeighborSearchOnKDTree<>( treeJ );
-
-					final RealCursor< DoubleType > cursorI = dataI.localizingCursor();
-
-					while ( cursorI.hasNext() )
+					if ( !genes.contains( gene ) )
 					{
-						cursorI.fwd();
-						searchJ.search( cursorI );
+						boolean validGene = true;
 
-						if ( searchJ.getDistance() < maxDistance )
+						// check that this gene is available for all pucks
+						for ( final STData data : puckData )
 						{
-							final double valueI = cursorI.get().get();
-							final double valueJ = searchJ.getSampler().get().get();
-
-							if ( valueI > 0 && valueJ > 0 )
-									localMatches.add(
-											new PointMatch(
-													new Point( new double[] { valueI } ),
-													new Point( new double[] { valueJ } ) ) );
+							if ( !new HashSet<>( data.getGeneNames() ).contains( gene ) )
+							{
+								validGene = false;
+								break;
+							}
 						}
+
+						if ( validGene )
+							genes.add( gene );
 					}
 				}
+			}
+		}
 
-				System.out.println( i + "-" + j + ": Found " + localMatches.size() + " corresponding measures. (" + siftmatches.genes.size() + " genes)" );
+		//genes.clear();
+		//genes.add( "Calm2" );
+		System.out.println( "Genes to be used: " + genes.size() );
+
+		final ExecutorService service = Threads.createFixedExecutorService( nThreads );
+
+		final HashMap< Pair< Integer, Integer >, ArrayList< PointMatch > > intensityMatches = new HashMap<>();
+
+		for ( int i = 0; i < pucks.size() - 1; ++i )
+		{
+			for ( int j = i + 1; j < pucks.size(); ++j )
+			{
+				final int i0 = i;
+				final int j0 = j;
+
+				// load the matches
+				final Matches siftmatches = GlobalOptSIFT.loadMatches( n5, pucks.get( i ), pucks.get( j ) );
+
+				if ( siftmatches.numInliers == 0 )
+					continue;
+
+				final List< Callable< ArrayList< PointMatch > > > tasks = new ArrayList<>();
+
+				// use all genes that were used for alignment (the same genes for all pairs, otherwise this is useless)
+				for ( final String gene : genes )
+				{
+					tasks.add( () ->
+					{
+						final ArrayList< PointMatch > threadMatches = new ArrayList<>();
+
+						// create two transformed IterableRealIntervals
+						IterableRealInterval< DoubleType > dataI = puckData.get( i0 ).getExprData( gene );
+						IterableRealInterval< DoubleType > dataJ = puckData.get( j0 ).getExprData( gene );
+	
+						dataI = Filters.filter( dataI, new GaussianFilterFactory<>( new DoubleType( 0 ), maxDistance * 4, WeightType.BY_SUM_OF_WEIGHTS ) );
+						dataJ = Filters.filter( dataJ, new GaussianFilterFactory<>( new DoubleType( 0 ), maxDistance * 4, WeightType.BY_SUM_OF_WEIGHTS ) );
+
+						final KDTree< DoubleType > treeJ = new KDTree<>( dataJ );
+						final NearestNeighborSearchOnKDTree< DoubleType > searchJ = new NearestNeighborSearchOnKDTree<>( treeJ );
+	
+						final RealCursor< DoubleType > cursorI = dataI.localizingCursor();
+	
+						while ( cursorI.hasNext() )
+						{
+							cursorI.fwd();
+							final double valueI = cursorI.get().get();
+
+							if ( valueI > 0.25 )
+							{
+								searchJ.search( cursorI );
+		
+								if ( searchJ.getDistance() < maxDistance )
+								{
+									final double valueJ = searchJ.getSampler().get().get();
+		
+									if ( valueI + valueJ > 0.5 && valueJ > 0.25 )
+										threadMatches.add(
+													new PointMatch(
+															new Point( new double[] { valueI } ),
+															new Point( new double[] { valueJ } ),
+															valueI * valueJ ) );
+								}
+							}
+						}
+
+						return threadMatches;
+					});
+				}
+
+				// corresponding intensity values
+				final ArrayList< PointMatch > localMatches = new ArrayList<>();
+
+				try
+				{
+					final List< Future< ArrayList< PointMatch > > > futures = service.invokeAll( tasks );
+					for ( final Future< ArrayList< PointMatch > > future : futures )
+						localMatches.addAll( future.get() );
+				}
+				catch ( final InterruptedException | ExecutionException e )
+				{
+					e.printStackTrace();
+					throw new RuntimeException( e );
+				}
+
+				System.out.println( new Date( System.currentTimeMillis() ) + ": " + i + "-" + j + ": Found " + localMatches.size() + " corresponding measures (using max " + maxMatches + ")" );
 
 				if ( localMatches.size() > 0 )
 					intensityMatches.put( new ValuePair< Integer, Integer >( i, j ), localMatches );
 			}
 		}
 
+		service.shutdown();
+
 		// cut every pair to a max number of matches
 		final Random rnd = new Random( 344 );
 		for ( final Entry< Pair< Integer, Integer >, ArrayList< PointMatch > > matches : intensityMatches.entrySet() )
 		{
-			while ( matches.getValue().size() > maxMatches )
-				matches.getValue().remove( rnd.nextInt( matches.getValue().size() ) );
+			Collections.sort( matches.getValue(), new Comparator<PointMatch>() {
+				@Override
+				public int compare(PointMatch o1, PointMatch o2) {
+					if ( o1.getWeight() == o2.getWeight() )
+						return 0;
+					else if ( o1.getWeight() < o2.getWeight() )
+						return 1;
+					else
+						return -1;
+				}
+			});
 
-			System.out.println( matches.getKey().getA() + "-" + matches.getKey().getB() + ": Found " + matches.getValue().size() + " corresponding measures." );
+			ArrayList< PointMatch > newList = new ArrayList<>();
+			for ( int i = 0; i < Math.min( maxMatches * 2, matches.getValue().size() ) ; ++i )
+				newList.add( matches.getValue().get( i ) );
 
-			for ( final PointMatch pm : matches.getValue() )
+			ArrayList< PointMatch > inliers = new ArrayList<>();
+
+			try {
+				new AffineModel1D().filterRansac( newList, inliers, 10000, 1, 0.1 );
+			} catch (NotEnoughDataPointsException e) {}
+
+			matches.getValue().clear();
+
+			double sumWeights = 0;
+			if ( inliers.size() > 0 )
 			{
-				System.out.println( pm.getP1().getL()[ 0 ] + " == " + pm.getP2().getL()[ 0 ] );
+				for ( int i = 0; i < Math.min( maxMatches - 1, inliers.size() ) ; ++i )
+				{
+					matches.getValue().add( inliers.get( i ) );
+					sumWeights += inliers.get( i ).getWeight();
+				}
 			}
+			else
+			{
+				for ( int i = 0; i < Math.min( maxMatches - 1, newList.size() ) ; ++i )
+				{
+					matches.getValue().add( newList.get( i ) );
+					sumWeights += newList.get( i ).getWeight();
+				}
+			}
+
+			System.out.println( matches.getKey().getA() + "-" + matches.getKey().getB() + ": " + newList.size() + ", " + inliers.size() + ", " + sumWeights );
+			/*
+			while ( matches.getValue().size() > maxMatches - 1 )
+				matches.getValue().remove( rnd.nextInt( matches.getValue().size() ) );
+			*/
+
+			// add an identity match, 0 should be 0, with the weight being equal to the number of matches
+			matches.getValue().add( new PointMatch( new Point( new double[] { 0 } ), new Point( new double[] { 0 } ), sumWeights*100 ) );
+
+			/*
+			if ( matches.getKey().getA() == 3 ||  matches.getKey().getB() == 3)
+			{
+				System.out.println( matches.getKey().getA() + "-" + matches.getKey().getB() + ": Found " + matches.getValue().size() + " corresponding measures." );
+
+				for ( final PointMatch pm : matches.getValue() )
+					System.out.println( pm.getP1().getL()[ 0 ] + " == " + pm.getP2().getL()[ 0 ] );
+			}*/
 		}
 
+
 		// global optimization
+		final double lambda1 = 0.01;
+		final double lambda2 = 0.01;
+
 		final HashMap< Integer, AffineModel1D > models =
 				globalOpt(
 						intensityMatches,
-						new InterpolatedAffineModel1D<AffineModel1D, TranslationModel1D >( new AffineModel1D(), new TranslationModel1D(), 0.9 ),
+						new InterpolatedAffineModel1D<>(
+								new InterpolatedAffineModel1D< AffineModel1D, TranslationModel1D >(
+										new AffineModel1D(), new TranslationModel1D(), lambda1 ),
+								new IdentityModel(), lambda2 ),
+						//new InterpolatedAffineModel1D<AffineModel1D, TranslationModel1D >( new AffineModel1D(), new TranslationModel1D(), 0.9 ),
 						//new AffineModel1D(),
 						//new TranslationModel1D(),
-						0.01,
-						1000 ); 
+						0.1,
+						2000 ); 
 
-		System.out.println();
-		System.out.println();
+		//System.out.println();
+		//System.out.println();
 
 		for ( final Entry< Pair< Integer, Integer >, ArrayList< PointMatch > > matches : intensityMatches.entrySet() )
 		{
-			System.out.println( matches.getKey().getA() + "-" + matches.getKey().getB() );
+			//System.out.println( matches.getKey().getA() + "-" + matches.getKey().getB() );
 
 			for ( final PointMatch pm : matches.getValue() )
 			{
@@ -142,7 +301,7 @@ public class IntensityAdjustment
 				models.get( matches.getKey().getA() ).applyInPlace( p1 );
 				models.get( matches.getKey().getB() ).applyInPlace( p2 );
 
-				System.out.println( p1[ 0 ] + " == " + p2[ 0 ] );
+				//System.out.println( p1[ 0 ] + " == " + p2[ 0 ] );
 			}
 		}
 
@@ -188,7 +347,7 @@ public class IntensityAdjustment
 		}
 
 		// fix a random tile
-		tc.fixTile( tiles.get( ids.iterator().next() ) );
+		//tc.fixTile( tiles.get( ids.iterator().next() ) );
 
 		try 
 		{
@@ -219,20 +378,52 @@ public class IntensityAdjustment
 		final HashMap< Integer, AffineModel1D > result = new HashMap<>();
 
 		final double[] array = new double[ 2 ];
-		double minOffset = Double.MAX_VALUE;
+		double avgScale = 0;
 
 		for ( final int id : ids )
 		{
 			final Tile< M > tile = tiles.get( id );
 			tile.getModel().toArray( array );
+			final AffineModel1D modelView = new AffineModel1D();
+			modelView.set( array[ 0 ], array[ 1 ] );
+			result.put( id, modelView );
 
+			avgScale += array[ 0 ];
+		}
+
+		avgScale /= ids.size();
+		System.out.println( "(" + new Date( System.currentTimeMillis() ) + "): Avg scale (will be corrected to avoid compression/expansion): " + avgScale );
+
+		final AffineModel1D scale = new AffineModel1D();
+		scale.set( 1.0 / avgScale, 0 );
+
+		for ( final AffineModel1D m : result.values() )
+		{
+			m.preConcatenate( scale );
+		}
+
+		double minOffset = Double.MAX_VALUE;
+
+		for ( final AffineModel1D m : result.values() )
+		{
+			m.toArray( array );
 			minOffset = Math.min( minOffset, array[ 1 ] );
 		}
 
-		System.out.println( "(" + new Date( System.currentTimeMillis() ) + "): Min offset (will be corrected to avoid negative intensities: " + minOffset );
-		System.out.println( "(" + new Date( System.currentTimeMillis() ) + "): Intensity adjustments:" );
+		System.out.println( "(" + new Date( System.currentTimeMillis() ) + "): Min offset (will be corrected to avoid negative intensities and offsets: " + minOffset );
+		//System.out.println( "(" + new Date( System.currentTimeMillis() ) + "): Intensity adjustments:" );
 
-		for ( final int id : ids )
+		for ( final Entry<Integer, AffineModel1D> e : result.entrySet() )
+		{
+			e.getValue().toArray( array );
+			array[ 1 ] -= minOffset;
+			e.getValue().set( array[ 0 ], array[ 1 ] );
+
+			//System.out.println( e.getKey() + ": " + Util.printCoordinates( array )  );
+		}
+
+		/*
+		for ( final AffineModel1D m : result.values() )
 		{
 			final Tile< M > tile = tiles.get( id );
 			tile.getModel().toArray( array );
@@ -242,7 +433,7 @@ public class IntensityAdjustment
 			result.put( id, modelView );
 
 			System.out.println( id + ": " + Util.printCoordinates( array )  );
-		}
+		}*/
 
 		return result;
 	}
@@ -261,10 +452,20 @@ public class IntensityAdjustment
 		}
 	}
 
+	public static double avgMedianDist( Collection< STDataStatistics > puckDataStatistics )
+	{
+		double avg = 0;
+
+		for ( final STDataStatistics s : puckDataStatistics )
+			avg += s.getMedianDistance();
+
+		return avg / (double)puckDataStatistics.size();
+	}
+
 	public static void main( String[] args ) throws IOException
 	{
 		final String path = Path.getPath();
-		final File n5File = new File( path + "slide-seq-normalized.n5" );
+		final File n5File = new File( path + "slide-seq-test.n5" );
 		final N5FSReader n5 = N5IO.openN5( n5File );
 		final List< String > pucks = N5IO.listAllDatasets( n5 );
 
@@ -283,20 +484,32 @@ public class IntensityAdjustment
 			transforms.add( t );
 		}
 
-		final double maxDistance = 20;
-		final int maxMatches = 300;
+		final double maxDistance = avgMedianDist( puckDataStatistics );
+		final int maxMatches = 1000;
+		final boolean correctScaling = true;
+		final int nThreads = Runtime.getRuntime().availableProcessors() / 2;
 
-		final HashMap< Integer, AffineModel1D > models = adjustIntensities( n5, pucks, puckData, transforms, maxDistance, maxMatches );
+		System.out.println( "max dist for assignment = " + maxDistance );
+
+		long time = System.currentTimeMillis();
+
+		final HashMap< Integer, AffineModel1D > models = adjustIntensities( n5, pucks, puckData, transforms, maxDistance, maxMatches, correctScaling, nThreads );
+
+		System.out.println( "took " + (System.currentTimeMillis() - time ) + " msec." );
 
 		final N5FSWriter n5Writer = N5IO.openN5write( n5File );
 
 		for ( int i = 0; i < pucks.size(); ++i )
 		{
-			final AffineModel1D model = models.get( i );
+			AffineModel1D model = models.get( i );
+
+			if ( model == null ) // wasn't connected
+				model = new AffineModel1D();
 
 			final String groupName = n5Writer.groupPath( pucks.get( i ) );
 			final double[] array = new double[ 2 ];
 			model.toArray( array );
+
 			n5Writer.setAttribute( groupName, "intensity_transform", array );
 
 			System.out.println( pucks.get( i ) + ": " + model );
