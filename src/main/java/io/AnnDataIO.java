6d4d322ad0a503087215d90f0de46df5d396ba71
Michael Innerberger <innerbergerm@hhmi.org>
2023-02-15 20:56:42 -0500

    Add method to read celltype ids/labels

--- src/main/java/io/AnnDataIO.java
+++ src/main/java/io/AnnDataIO.java
@@ -3,12 +3,14 @@ package io;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
 import ch.systemsx.cisd.hdf5.HDF5Factory;
 import ch.systemsx.cisd.hdf5.IHDF5Reader;
 import data.STDataText;
+import net.imglib2.img.array.ArrayImgs;
 import net.imglib2.util.Pair;
 import net.imglib2.util.ValuePair;
 import org.janelia.saalfeldlab.n5.DatasetAttributes;
@@ -18,6 +20,8 @@ import data.STData;
 
 public class AnnDataIO
 {
+	private static final String celltypePath = "/obs/cell_type";
+	private static final String locationPath = "/obsm/locations";
 
 	public static void main( String[] args ) throws IOException
 	{
@@ -27,7 +31,6 @@ public class AnnDataIO
 		final N5HDF5Reader n5Reader = new N5HDF5Reader(path);
 
 		final File file = new File(path);
-		System.out.println("Does the dataset contain celltypes? " + containsCelltypes(file));
 		STData stData = readSlideSeq(file);
 	}
 
@@ -36,12 +39,17 @@ public class AnnDataIO
 		N5HDF5Reader n5Reader = new N5HDF5Reader(anndataFile.getAbsolutePath());
 
 		final List<Pair<double[], String>> coordinateList = readSlideSeqCoordinates(n5Reader);
-		System.out.println("Read " + coordinateList.size() + " coordinates.");
+		System.out.println("Read " + coordinateList.size() + " coordinates from '" + locationPath + "'.");
 
 		final HashMap<String, double[]> geneData = readSlideSeqGenes(n5Reader);
-		System.out.println("Read data for " + geneData.keySet().size() + " genes.");
+		System.out.println("Read data for " + geneData.keySet().size() + " genes from '/X'.");
 
 		final STData data = new STDataText(coordinateList, geneData);
+		if (containsCelltypes(anndataFile)) {
+			int[] celltypeIds = getCelltypeIds(n5Reader);
+			data.getMetaData().put("celltype", ArrayImgs.ints(celltypeIds, (int)data.numLocations()));
+			System.out.println("Loading '" + celltypePath + "' as label 'celltype'.");
+		}
 
 		System.out.println("Parsing took " + (System.currentTimeMillis() - time) + " ms.");
 
@@ -88,7 +96,7 @@ public class AnnDataIO
 		String[] barcodes = hdf5Reader.readStringArray("obs/_index");
 		int numCoordinates = barcodes.length;
 
-		DatasetAttributes attributes = n5Reader.getDatasetAttributes("obsm/locations");
+		DatasetAttributes attributes = n5Reader.getDatasetAttributes(locationPath);
 
 		if (numCoordinates != (int) attributes.getDimensions()[1])
 			throw new RuntimeException("Number of barcodes does not match number of coordinates.");
@@ -96,7 +104,7 @@ public class AnnDataIO
 		// coordinates are stored row-wise in python
 		final ArrayList<Pair<double[], String>> coordinates = new ArrayList<>();
 		final int dim = attributes.getNumDimensions();
-		double[] block = (double[]) n5Reader.readBlock("obsm/locations", attributes, 0, 0).getData();
+		double[] block = (double[]) n5Reader.readBlock(locationPath, attributes, 0, 0).getData();
 
 		for ( int i=0; i<numCoordinates; ++i ) {
 			final double[] coordinate = new double[dim];
@@ -130,10 +138,29 @@ public class AnnDataIO
 
 	public static Boolean containsCelltypes(final File anndataFile) {
 		try (final N5HDF5Reader n5Reader = new N5HDF5Reader(anndataFile.getAbsolutePath())) {
-			return n5Reader.exists("/obs/cell_type");
+			return n5Reader.exists(celltypePath);
 		}
 		catch (IOException e) {
 			return Boolean.FALSE;
 		}
+	}
+
+	public static int[] getCelltypeIds(final N5HDF5Reader n5Reader) throws IOException {
+		// TODO: not all celltypes are stored as categorical
+		final DatasetAttributes attributes = n5Reader.getDatasetAttributes(celltypePath + "/codes");
+		final byte[] celltypeIdsByte = (byte[]) n5Reader.readBlock(celltypePath + "/codes", attributes, 0).getData();
+
+		final int[] celltypeIds = new int[celltypeIdsByte.length];
+		for (int i=0; i<celltypeIds.length; ++i)
+			celltypeIds[i] = (int)celltypeIdsByte[i];
+
+		// this assembles the actual type names
+		// final IHDF5Reader hdf5Reader = HDF5Factory.openForReading(n5Reader.getFilename());
+		// final String[] categoryNames = hdf5Reader.readStringArray(celltypePath + "/categories");
+		// final String[] celltypeNames = new String[celltypeIds.length];
+		// for (int i=0; i<celltypeIds.length; ++i)
+		// 	celltypeNames[i] = categoryNames[celltypeIds[i]];
+
+		return celltypeIds;
 	}
 }
