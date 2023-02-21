83d1bbb328b2a80c2265e186ab4d62c602f8adeb
Michael Innerberger <innerbergerm@hhmi.org>
2023-02-21 16:46:45 -0500

    Add categorical arrays

--- src/main/java/anndata/CategoricalArray.java
+++ src/main/java/anndata/CategoricalArray.java
@@ -1,32 +1,36 @@
 package anndata;
 
 import java.util.Arrays;
+import java.util.stream.IntStream;
 
-public class StringArray extends AbstractAnnDataField<String, String[]> {
+public class CategoricalArray extends AbstractAnnDataField<String, String[]> {
 
-    public StringArray(final int numRows, final int numColumns, final String[] data) {
+    protected final int[] indices;
+
+    protected CategoricalArray(int numRows, int numColumns, String[] data, int[] indices) {
         super(numRows, numColumns, data);
+        this.indices = indices;
 
-        if ((numRows != 1 && numColumns != 1) || (numRows != data.length && numColumns != data.length))
-            throw new IllegalArgumentException("String arrays can only be one dimensional");
+        if ((numRows != 1 && numColumns != 1) || (numRows != indices.length && numColumns != indices.length))
+            throw new IllegalArgumentException("Categorical arrays can only be one dimensional");
     }
 
     @Override
     public String get(final int i, final int j) {
         if (i != 0 && j != 0)
-            throw new IllegalArgumentException("String arrays can only be one dimensional");
+            throw new IllegalArgumentException("Categorical arrays can only be one dimensional");
 
         return this.get(i+j);
     }
 
     public String get(final int i) {
-        return data[i];
+        return data[indices[i]];
     }
 
     @Override
     public String[] getRow(final int i) {
         if (numRows == 1)
-            return Arrays.copyOf(data, data.length);
+            return IntStream.of(indices).mapToObj(k -> data[k]).toArray(String[]::new);
         else
             return new String[]{get(i)};
     }
@@ -34,13 +38,13 @@ public class StringArray extends AbstractAnnDataField<String, String[]> {
     @Override
     public String[] getColumn(final int j) {
         if (numColumns == 1)
-            return Arrays.copyOf(data, data.length);
+            return IntStream.of(indices).mapToObj(k -> data[k]).toArray(String[]::new);
         else
             return new String[]{get(j)};
     }
 
     public static void main(String[] args) {
-        StringArray A = new StringArray(1, 4, new String[]{"", "A", "B", ":-þ"});
+        CategoricalArray A = new CategoricalArray(1, 5, new String[]{"A", "B", "ð"}, new int[]{2, 1, 1, 0, 2});
         System.out.println("A(0,1) = " + A.get(0,1));
 
         System.out.print("Row 0: ");
