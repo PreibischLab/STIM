fd07c0a33516a951adf3ac0e11674359c5399ba9
Michael Innerberger <innerbergerm@hhmi.org>
2023-03-17 16:37:59 -0400

    Refine tests for sparse image

--- src/test/java/anndata/SparseImageTest.java
+++ src/test/java/anndata/SparseImageTest.java
@@ -15,6 +15,7 @@ import net.imglib2.img.array.ArrayImgFactory;
 import net.imglib2.type.Type;
 import net.imglib2.type.numeric.integer.LongType;
 import net.imglib2.type.numeric.real.DoubleType;
+import org.junit.Before;
 import org.junit.Test;
 
 
@@ -23,9 +24,12 @@ public class SparseImageTest {
 	private Img<DoubleType> data;
 	private Img<LongType> indices;
 	private Img<LongType> indptr;
+	private AbstractCompressedStorageRai<DoubleType, LongType> csr;
+	private AbstractCompressedStorageRai<DoubleType, LongType> csc;
+	private List<AbstractCompressedStorageRai<DoubleType, LongType>> sparseImgs;
 
-	@Test
-	public void correctNumberOfNonzeros() {
+	@Before
+	public void setupSparseImages() {
 		data = create1DImgFromList(new ArrayImgFactory<>(new DoubleType()),
 				Stream.of(1.0, 1.0, 1.0, 1.0, 1.0).map(DoubleType::new).collect(Collectors.toList()));
 		indices = create1DImgFromList(new ArrayImgFactory<>(new LongType()),
@@ -33,24 +37,46 @@ public class SparseImageTest {
 		indptr = create1DImgFromList(new ArrayImgFactory<>(new LongType()),
 				Stream.of(0, 1, 2, 3, 3, 3, 3, 3, 3, 5).map(LongType::new).collect(Collectors.toList()));
 
-		AbstractCompressedStorageRai<DoubleType, LongType> sparse = new CsrRandomAccessibleInterval<>(10, 9, data, indices, indptr);
-		RandomAccess<DoubleType> ra = sparse.randomAccess();
+		csr = new CsrRandomAccessibleInterval<>(10, 9, data, indices, indptr);
+		csc = new CscRandomAccessibleInterval<>(9, 10, data, indices, indptr);
+		sparseImgs = Stream.of(csr, csc).collect(Collectors.toList());
+	}
+
+	@Test
+	public void csrSetupIsCorrect() {
+		assertEquals(2, csr.numDimensions());
+		assertArrayEquals(new long[]{0, 0}, csr.minAsLongArray());
+		assertArrayEquals(new long[]{9, 8}, csr.maxAsLongArray());
+
+		RandomAccess<DoubleType> ra = csr.randomAccess();
 		assertEquals(1.0, ra.setPositionAndGet(2,0).getRealDouble(), 1e-6);
 		assertEquals(1.0, ra.setPositionAndGet(5,1).getRealDouble(), 1e-6);
 		assertEquals(1.0, ra.setPositionAndGet(0,2).getRealDouble(), 1e-6);
 		assertEquals(1.0, ra.setPositionAndGet(6,8).getRealDouble(), 1e-6);
 		assertEquals(1.0, ra.setPositionAndGet(9,8).getRealDouble(), 1e-6);
+	}
 
+	@Test
+	public void sparseHasCorrectNumberOfNonzeros() {
+		for (AbstractCompressedStorageRai<DoubleType, LongType> sparse : sparseImgs) {
+			int count = 0;
+			RandomAccess<DoubleType> ra = sparse.randomAccess();
+			for (int i = 0; i <= sparse.max(0); ++i)
+				for (int j = 0; j <= sparse.max(1); ++j)
+					if (ra.setPositionAndGet(i, j).getRealDouble() != 0.0)
+						++count;
 
-		assertEquals(2, sparse.numDimensions());
-		assertArrayEquals(new long[]{0, 0}, sparse.minAsLongArray());
-		int count = 0;
-		for (int i = 0; i <= sparse.max(0); ++i)
-			for (int j = 0; j <= sparse.max(1); ++j)
-				if (ra.setPositionAndGet(i, j).getRealDouble() != 0.0)
-					++count;
-		assertEquals(data.max(0)+1, count);
+			assertEquals(data.max(0)+1, count);
+		}
+	}
 
+	@Test
+	public void cscIsCsrTransposed() {
+		RandomAccess<DoubleType> raR = csr.randomAccess();
+		RandomAccess<DoubleType> raC = csc.randomAccess();
+		for (int i = 0; i <= csr.max(0); ++i)
+			for (int j = 0; j <= csr.max(1); ++j)
+				assertEquals(raR.setPositionAndGet(i, j), raC.setPositionAndGet(j, i));
 	}
 
 	private <T extends Type<T>> Img<T> create1DImgFromList(ImgFactory<T> imgFactory, List<T> values) {
