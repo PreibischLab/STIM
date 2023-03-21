9f87103f10d24d639cc206dca7f0c2e589a1114c
Michael Innerberger <innerbergerm@hhmi.org>
2023-03-21 15:23:31 -0400

    Migrate tests to JUnit5

--- src/test/java/FilterTest.java
+++ src/test/java/FilterTest.java
@@ -8,58 +8,59 @@ import net.imglib2.KDTree;
 import net.imglib2.RealPoint;
 import net.imglib2.RealPointSampleList;
 import net.imglib2.type.numeric.real.DoubleType;
-import org.junit.Assert;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Named;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.BiFunction;
+import java.util.stream.Stream;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Named.named;
 
-@RunWith(Parameterized.class)
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 public class FilterTest {
 
-	@Parameterized.Parameter
-	public BiFunction<Double, KDTree<DoubleType>, Filter<DoubleType>> filterCreator;
-
-	@Parameterized.Parameters(name = "FilterFactory {index}")
-    public static Iterable<?> data() {
-		List<BiFunction<Double, KDTree<DoubleType>, Filter<DoubleType>>> parameters = Arrays.asList(
-			(radius, tree) -> new DensityFilterFactory<>(new DoubleType(0), radius).createFilter(tree),
-			(radius, tree) -> new MedianFilterFactory<>(new DoubleType(0), radius).createFilter(tree),
-			(radius, tree) -> new SingleSpotRemovingFilterFactory<>(new DoubleType(0), radius).createFilter(tree),
-			(radius, tree) -> new MeanFilterFactory<DoubleType, DoubleType>(new DoubleType(0), radius).createFilter(tree),
-			(radius, tree) -> new GaussianFilterFactory<DoubleType, DoubleType>(new DoubleType(0), radius).createFilter(tree));
-		return parameters;
+    public static Stream<Named<DoubleFilterCreator>> provideParameters() {
+		return Stream.of(
+			named("Density Filter", (radius, tree) -> new DensityFilterFactory<>(new DoubleType(0), radius).createFilter(tree)),
+			named("Median Filter", (radius, tree) -> new MedianFilterFactory<>(new DoubleType(0), radius).createFilter(tree)),
+			named("Single Spot Removing Filter", (radius, tree) -> new SingleSpotRemovingFilterFactory<>(new DoubleType(0), radius).createFilter(tree)),
+			named("Mean Filter", (radius, tree) -> new MeanFilterFactory<DoubleType, DoubleType>(new DoubleType(0), radius).createFilter(tree)),
+			named("Gaussian Filter", (radius, tree) -> new GaussianFilterFactory<DoubleType, DoubleType>(new DoubleType(0), radius).createFilter(tree)));
     }
 
-	@Test
-	public void filterIsInvariantWrtRadius() {
+	@ParameterizedTest
+	@MethodSource("provideParameters")
+	public void filter_is_ivariant_wrt_radius(DoubleFilterCreator filterCreator) {
 		KDTree<DoubleType> tree = createSingleNodeTree();
 		RealPoint queryPoint = new RealPoint(0.0, 0.0);
 
 		DoubleType expectedValue = new DoubleType(0.0);
-		filterCreator.apply(1e-14, tree).filter(queryPoint, expectedValue);
+		filterCreator.create(1e-14, tree).filter(queryPoint, expectedValue);
 		DoubleType actualValue = new DoubleType(0.0);
 
 		for (double radius = 1e-12; radius < 1e1; radius *= 1e3) {
-			Filter<DoubleType> filter = filterCreator.apply(radius, tree);
+			Filter<DoubleType> filter = filterCreator.create(radius, tree);
 			filter.filter(queryPoint, actualValue);
 			assertEquals(expectedValue.getRealDouble(), actualValue.getRealDouble(), 1e-8);
 		}
 	}
 
-	@Test
-	public void filterIsLocal() {
+	@ParameterizedTest
+	@MethodSource("provideParameters")
+	public void filter_is_local(DoubleFilterCreator filterCreator) {
 		KDTree<DoubleType> tree = createSingleNodeTree();
 		List<RealPoint> queryPoints = Arrays.asList(
 				new RealPoint(-1.0, -1.0), new RealPoint(  1.0, -1.0),
 				new RealPoint( 1.0,  1.0), new RealPoint( -1.0,  1.0));
 
-		Filter<DoubleType> filter = filterCreator.apply(0.1, tree);
+		Filter<DoubleType> filter = filterCreator.create(0.1, tree);
 		DoubleType actualValue = new DoubleType(0.0);
 
 		for (RealPoint point : queryPoints) {
@@ -68,16 +69,17 @@ public class FilterTest {
 		}
 	}
 
-	@Test
-	public void filterRespectsNeighborhood() {
+	@ParameterizedTest
+	@MethodSource("provideParameters")
+	public void filter_respects_neighborhood(DoubleFilterCreator filterCreator) {
 		KDTree<DoubleType> tree = createDoubleNodeTree();
 		RealPoint queryPoint = new RealPoint(0.0, 0.0);
 
-		Filter<DoubleType> filter = filterCreator.apply(1.0, tree);
+		Filter<DoubleType> filter = filterCreator.create(1.0, tree);
 		DoubleType actualValue = new DoubleType(0.0);
 		filter.filter(queryPoint, actualValue);
 
-		Assert.assertTrue(0.0 < actualValue.getRealDouble());
+		assertTrue(0.0 < actualValue.getRealDouble());
 	}
 
 	protected KDTree<DoubleType> createSingleNodeTree() {
@@ -91,5 +93,9 @@ public class FilterTest {
 		samples.add(new RealPoint( 0.5, 0.0), new DoubleType(1.0));
 		samples.add(new RealPoint(-0.5, 0.0), new DoubleType(1.0));
 		return new KDTree<>(samples);
+	}
+
+	protected interface DoubleFilterCreator {
+		Filter<DoubleType> create(double radius, KDTree<DoubleType> tree);
 	}
 }
--- src/test/java/SparseImageTest.java
+++ src/test/java/SparseImageTest.java
@@ -1,79 +1,99 @@
+import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertArrayEquals;
 
 import anndata.AbstractCompressedStorageRai;
 import anndata.CscRandomAccessibleInterval;
 import anndata.CsrRandomAccessibleInterval;
 import net.imglib2.RandomAccess;
 import net.imglib2.img.Img;
+import net.imglib2.img.ImgFactory;
 import net.imglib2.img.array.ArrayImgFactory;
+import net.imglib2.type.Type;
 import net.imglib2.type.numeric.integer.LongType;
 import net.imglib2.type.numeric.real.DoubleType;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Named;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Named.named;
 
 
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 public class SparseImageTest {
 
-	private Img<DoubleType> data;
-	private Img<LongType> indices;
-	private Img<LongType> indptr;
-	private AbstractCompressedStorageRai<DoubleType, LongType> csr;
-	private AbstractCompressedStorageRai<DoubleType, LongType> csc;
-	private List<AbstractCompressedStorageRai<DoubleType, LongType>> sparseImgs;
-
-	@Before
-	public void setupSparseImages() {
-		data = TestUtils.create1DImgFromList(new ArrayImgFactory<>(new DoubleType()),
-				Stream.of(1.0, 1.0, 1.0, 1.0, 1.0).map(DoubleType::new).collect(Collectors.toList()));
-		indices = TestUtils.create1DImgFromList(new ArrayImgFactory<>(new LongType()),
-				Stream.of(2, 5, 0, 6, 9).map(LongType::new).collect(Collectors.toList()));
-		indptr = TestUtils.create1DImgFromList(new ArrayImgFactory<>(new LongType()),
-				Stream.of(0, 1, 2, 3, 3, 3, 3, 3, 3, 5).map(LongType::new).collect(Collectors.toList()));
-
-		csr = new CsrRandomAccessibleInterval<>(10, 9, data, indices, indptr);
-		csc = new CscRandomAccessibleInterval<>(9, 10, data, indices, indptr);
-		sparseImgs = Stream.of(csr, csc).collect(Collectors.toList());
-	}
-
 	@Test
-	public void csrSetupIsCorrect() {
+	public void CSR_setup_is_correct() {
+		CsrRandomAccessibleInterval<DoubleType, LongType> csr = setupCsr();
 		assertEquals(2, csr.numDimensions());
 		assertArrayEquals(new long[]{0, 0}, csr.minAsLongArray());
 		assertArrayEquals(new long[]{9, 8}, csr.maxAsLongArray());
+	}
 
-		RandomAccess<DoubleType> ra = csr.randomAccess();
-		assertEquals(1.0, ra.setPositionAndGet(2,0).getRealDouble(), 1e-6);
-		assertEquals(1.0, ra.setPositionAndGet(5,1).getRealDouble(), 1e-6);
-		assertEquals(1.0, ra.setPositionAndGet(0,2).getRealDouble(), 1e-6);
-		assertEquals(1.0, ra.setPositionAndGet(6,8).getRealDouble(), 1e-6);
-		assertEquals(1.0, ra.setPositionAndGet(9,8).getRealDouble(), 1e-6);
+	@ParameterizedTest
+	@CsvSource({"2,0", "5,1", "0,2", "6,8", "9,8"})
+	public void CSR_nonzero_entries_are_correct(int x, int y) {
+		RandomAccess<DoubleType> ra = setupCsr().randomAccess();
+		assertEquals(1.0, ra.setPositionAndGet(x,y).getRealDouble(), 1e-6);
+	}
+
+	@ParameterizedTest
+	@MethodSource("setupSparseImages")
+	public void sparse_has_correct_number_of_nonzeros(AbstractCompressedStorageRai<DoubleType, LongType> sparse) {
+		int count = 0;
+		RandomAccess<DoubleType> ra = sparse.randomAccess();
+		for (int i = 0; i <= sparse.max(0); ++i)
+			for (int j = 0; j <= sparse.max(1); ++j)
+				if (ra.setPositionAndGet(i, j).getRealDouble() != 0.0)
+					++count;
+
+		assertEquals(5, count);
 	}
 
 	@Test
-	public void sparseHasCorrectNumberOfNonzeros() {
-		for (AbstractCompressedStorageRai<DoubleType, LongType> sparse : sparseImgs) {
-			int count = 0;
-			RandomAccess<DoubleType> ra = sparse.randomAccess();
-			for (int i = 0; i <= sparse.max(0); ++i)
-				for (int j = 0; j <= sparse.max(1); ++j)
-					if (ra.setPositionAndGet(i, j).getRealDouble() != 0.0)
-						++count;
-
-			assertEquals(data.max(0)+1, count);
-		}
-	}
-
-	@Test
-	public void cscIsCsrTransposed() {
+	public void CSC_is_CSR_transposed() {
+		CsrRandomAccessibleInterval<DoubleType, LongType> csr = setupCsr();
 		RandomAccess<DoubleType> raR = csr.randomAccess();
-		RandomAccess<DoubleType> raC = csc.randomAccess();
+		RandomAccess<DoubleType> raC = setupCsc().randomAccess();
 		for (int i = 0; i <= csr.max(0); ++i)
 			for (int j = 0; j <= csr.max(1); ++j)
 				assertEquals(raR.setPositionAndGet(i, j), raC.setPositionAndGet(j, i));
+	}
+
+	public CsrRandomAccessibleInterval<DoubleType, LongType> setupCsr() {
+		return (CsrRandomAccessibleInterval<DoubleType, LongType>) setupSparseImages().get(0).getPayload();
+	}
+
+	public CscRandomAccessibleInterval<DoubleType, LongType> setupCsc() {
+		return (CscRandomAccessibleInterval<DoubleType, LongType>) setupSparseImages().get(1).getPayload();
+	}
+
+	public static List<Named<AbstractCompressedStorageRai<DoubleType, LongType>>> setupSparseImages() {
+		Img<DoubleType> data = create1DImgFromList(new ArrayImgFactory<>(new DoubleType()),
+				Stream.of(1.0, 1.0, 1.0, 1.0, 1.0).map(DoubleType::new).collect(Collectors.toList()));
+		Img<LongType> indices = create1DImgFromList(new ArrayImgFactory<>(new LongType()),
+				Stream.of(2, 5, 0, 6, 9).map(LongType::new).collect(Collectors.toList()));
+		Img<LongType> indptr = create1DImgFromList(new ArrayImgFactory<>(new LongType()),
+				Stream.of(0, 1, 2, 3, 3, 3, 3, 3, 3, 5).map(LongType::new).collect(Collectors.toList()));
+
+		return Arrays.asList(
+				named("CSR", new CsrRandomAccessibleInterval<>(10, 9, data, indices, indptr)),
+				named("CSC", new CscRandomAccessibleInterval<>(9, 10, data, indices, indptr)));
+	}
+
+	protected static  <T extends Type<T>> Img<T> create1DImgFromList(ImgFactory<T> imgFactory, List<T> values) {
+		final Img<T> img = imgFactory.create(values.size());
+		final Iterator<T> valueIterator = values.iterator();
+		for (final T pixel : img)
+			pixel.set(valueIterator.next());
+		return img;
 	}
 }
