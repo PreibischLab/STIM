ba4a9c3e1d3be36186e3123e8ccb623c00b5e68b
Michael Innerberger <innerbergerm@hhmi.org>
2023-03-31 15:36:15 -0400

    Make functionality of spatial data group more robust

--- src/main/java/io/SpatialDataGroup.java
+++ src/main/java/io/SpatialDataGroup.java
@@ -6,6 +6,7 @@ import org.janelia.saalfeldlab.n5.N5FSWriter;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
@@ -64,13 +65,23 @@ public class SpatialDataGroup {
 		writer.setAttribute("/", "datasets", datasets.toArray());
 	}
 
-	public void addExistingDataset(String path, String datasetName) throws IOException {
-		Files.move(Paths.get(path, datasetName), Paths.get(rootPath, datasetName));
+	public void addExistingDataset(String path) throws IOException {
+		Path oldPath = Paths.get(path);
+		String datasetName = oldPath.getFileName().toString();
+
+		if (readOnly)
+			throw new SpatialDataIOException("Trying to modify a read-only spatial data group.");
+		if (datasets.contains(datasetName))
+			throw new SpatialDataIOException("Dataset '" + datasetName + "' already exists.");
+
+		Files.move(oldPath, Paths.get(rootPath, datasetName));
 		datasets.add(datasetName);
 		updateDatasetMetadata();
 	}
 
 	public void deleteDataset(String datasetName) throws IOException {
+		if (readOnly)
+			throw new SpatialDataIOException("Trying to modify a read-only spatial data group.");
 		Files.delete(Paths.get(rootPath, datasetName));
 		datasets.remove(datasetName);
 		updateDatasetMetadata();
--- src/test/java/SpatialDataGroupTest.java
+++ src/test/java/SpatialDataGroupTest.java
@@ -1,7 +1,7 @@
-import gui.STDataAssembly;
 import io.AnnDataIO;
 import io.SpatialDataGroup;
 import io.SpatialDataIO;
+import io.SpatialDataIOException;
 import org.janelia.saalfeldlab.n5.hdf5.N5HDF5Writer;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
@@ -9,11 +9,11 @@ import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
@@ -35,20 +35,35 @@ public class SpatialDataGroupTest extends AbstractIOTest {
 	@Test
 	public void adding_and_deleting_dataset_works() throws IOException {
 		SpatialDataGroup group = SpatialDataGroup.createNew(getPath());
-		STDataAssembly dataA = new STDataAssembly(TestUtils.createTestDataSet());
 
 		final String datasetPath = "data";
 		final String datasetName = "tmp.h5ad";
-		Path fullPath = Paths.get(datasetPath, datasetName);
-		SpatialDataIO sdio = new AnnDataIO(datasetPath, new N5HDF5Writer(fullPath.toString()));
-		group.addExistingDataset(datasetPath, datasetName);
-
-		List<String> datasets = group.getDatasets();
-		assertTrue(datasets.contains(datasetName));
+		String fullPath = Paths.get(datasetPath, datasetName).toString();
+		SpatialDataIO sdio = new AnnDataIO(fullPath, new N5HDF5Writer(fullPath));
+		group.addExistingDataset(fullPath);
 
 		File datasetFile = new File(Paths.get(getPath(), "tmp.h5ad").toString());
+		assertTrue(group.getDatasets().contains(datasetName));
 		assertTrue(datasetFile.exists());
+
 		group.deleteDataset(datasetName);
+		assertFalse(group.getDatasets().contains(datasetName));
 		assertFalse(datasetFile.exists());
+	}
+
+	@Test
+	public void adding_existing_dataset_fails() throws IOException {
+		SpatialDataGroup group = SpatialDataGroup.createNew(getPath());
+
+		final String datasetPath = "data";
+		final String datasetName = "tmp.h5ad";
+		String fullPath = Paths.get(datasetPath, datasetName).toString();
+		SpatialDataIO sdio = new AnnDataIO(fullPath, new N5HDF5Writer(fullPath));
+		group.addExistingDataset(fullPath);
+
+		assertThrows(SpatialDataIOException.class, () -> group.addExistingDataset(fullPath));
+
+		group.deleteDataset(datasetName);
+		(new File(fullPath)).delete();
 	}
 }
