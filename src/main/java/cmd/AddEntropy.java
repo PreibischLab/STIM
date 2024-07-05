1a0be9c42969f7e065d517e5c928da7eaa1f8adb
Daniel León-Perinán <daniel.leonperinan@mdc-berlin.de>
2024-07-05 11:56:52 +0200

    new command to add entropy as gene annotation

---
+++ src/main/java/cmd/AddEntropy.java
@@ -0,0 +1,60 @@
+package cmd;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import analyze.ExtractGeneLists;
+import gui.STDataAssembly;
+import io.SpatialDataIO;
+import net.imglib2.img.array.ArrayImg;
+import net.imglib2.img.array.ArrayImgs;
+import net.imglib2.img.basictypeaccess.array.DoubleArray;
+import net.imglib2.type.numeric.real.DoubleType;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+
+// In the future, this will support more methods for computing the std
+@Command(name = "st-add-entropy", mixinStandardHelpOptions = true, version = "0.3.0", description = "Spatial Transcriptomics as IMages project - add annotations to slice-dataset")
+public class AddEntropy implements Callable<Void> {
+
+	@Option(names = {"-i", "--input"}, required = true, description = "input dataset, e.g. -i /home/ssq.n5/Puck_180528_20")
+	private String inputPath = null;
+
+	@Option(names = {"-m", "--method"}, required = false, description = "method to compute gene entropy")
+	private String method = "stdev";
+
+	@Option(names = {"-gl", "--geneLabel"}, required = false, description = "custom label where to save the computed entropy")
+	private String geneLabels = "stdev";
+
+	@Option(names = {"--numThreads"}, required = false, description = "number of threads for parallel processing")
+	private int numThreads = 128;
+
+	@Override
+	public Void call() throws Exception {
+		if (inputPath == null) {
+			System.out.println("No input path defined. Stopping.");
+			return null;
+		}
+
+		final ExecutorService service = Executors.newFixedThreadPool(8);
+		final SpatialDataIO sdio = SpatialDataIO.open(inputPath, service);
+		final STDataAssembly stData = sdio.readData();
+		final ArrayImg<DoubleType, DoubleArray> entropy_values_rai;
+
+		final double[] entropy_values = ExtractGeneLists.computeEntropy(method, stData.data(), numThreads);
+		entropy_values_rai = ArrayImgs.doubles(entropy_values, (long) stData.data().numGenes());
+		stData.data().getGeneAnnotations().put(geneLabels, entropy_values_rai);
+		sdio.updateStoredGeneAnnotations(stData.data().getGeneAnnotations());
+	
+		System.out.println( "Done." );
+
+		service.shutdown();
+		return null;
+	}
+
+	public static final void main(final String... args) {
+		CommandLine.call(new AddEntropy(), args);
+	}
+}
