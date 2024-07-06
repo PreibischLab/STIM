18c08107d9f215d57e112a633768c6d4136ba457
Daniel León-Perinán <daniel.leonperinan@mdc-berlin.de>
2024-07-06 11:53:55 +0200

    better phrasing for entropyPath arg message

--- src/main/java/cmd/PairwiseSectionAligner.java
+++ src/main/java/cmd/PairwiseSectionAligner.java
@@ -93,7 +93,7 @@ public class PairwiseSectionAligner implements Callable<Void> {
 	@Option(names = {"-n", "--numGenes"}, required = false, description = "initial number of genes for alignment that have the highest entropy (default: 10)")
 	private int numGenes = 10;
 
-	@Option(names = {"--entropyPath"}, required = false, description = "path where the entropy is stored as gene annotations (default: compute standard deviation from scratch)")
+	@Option(names = {"--entropyPath"}, required = false, description = "path where the entropy is stored as gene annotations (if no path given: compute standard deviation from scratch)")
 	private String entropyPath = null;
 
 	@Option(names = {"-e", "--maxEpsilon"}, required = false, description = "maximally allowed alignment error (in global space, independent of scaling factor) for SIFT on a 2D rigid model (default: 10 times the average distance between sequenced locations)")
