aed83b2ae8a505b81a9de65ff6e7282623f1b418
Daniel León-Perinán <daniel.leonperinan@mdc-berlin.de>
2024-07-09 19:08:07 +0200

    support env variable for logging level

--- src/main/java/util/LoggerUtil.java
+++ src/main/java/util/LoggerUtil.java
@@ -7,12 +7,33 @@ import org.apache.logging.log4j.core.config.Configurator;
 
 public class LoggerUtil {
     private static final Logger logger = LogManager.getLogger("STIM");
+    private static final String LOG_LEVEL_ENV_VAR = "STIM_LOG_LEVEL";
+    private static final Level DEFAULT_LOG_LEVEL = Level.INFO;
+
+    static {
+        initializeLogLevel();
+    }
 
     private LoggerUtil() {}
 
-    public static void setLogLevel(String logLevel) {
-        Level level = Level.toLevel(logLevel, Level.INFO);
-        Configurator.setRootLevel(level);
+    private static void initializeLogLevel() {
+        String logLevelStr = System.getenv(LOG_LEVEL_ENV_VAR);
+        Level logLevel = parseLogLevel(logLevelStr);
+        Configurator.setRootLevel(logLevel);
+        logger.info("Log level set to: {}", logLevel);
+    }
+
+    private static Level parseLogLevel(String logLevelStr) {
+        if (logLevelStr == null || logLevelStr.isEmpty()) {
+            return DEFAULT_LOG_LEVEL;
+        }
+
+        try {
+            return Level.valueOf(logLevelStr.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            logger.warn("Invalid log level '{}' specified. Using default level: {}", logLevelStr, DEFAULT_LOG_LEVEL);
+            return DEFAULT_LOG_LEVEL;
+        }
     }
 
     public static Logger getLogger() {
