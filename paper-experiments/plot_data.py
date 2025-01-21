23507351641683b462f192f06e2833494edde64e
Michael Innerberger <innerbergerm@hhmi.org>
2025-01-21 11:07:40 -0500

    Modify plots to contain new data

--- paper-experiments/plot_data.py
+++ paper-experiments/plot_data.py
@@ -5,9 +5,16 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-NAMES = ['automatic', 'SP', 'NK', 'DLP', 'MI']
-COLORS = ['tab:grey', 'tab:blue', 'tab:green', 'tab:red', 'tab:purple']
+NAMES = ['automatic', 'SP', 'NK', 'DLP', 'MI', 'morpho', 'spacel']
+LABELS =['STIM', 'Human #1', 'Human #2', 'Human #3', 'Human #4', 'Morpho', 'Spacel']
+COLORS = ['tab:grey', 'tab:blue', 'tab:green', 'tab:red', 'tab:purple', 'tab:orange', 'tab:cyan']
+MICRON_PER_PIXEL = 0.345
+PAD = 5
+NUM_GROUPS = len(NAMES)
 script_dir = os.path.dirname(__file__)
+
+
+# ---- Pairwise errors ----
 
 data = []
 colors = []
@@ -26,8 +33,9 @@ for name1 in NAMES:
         data.append(df['meanError'])
 
 # plot pairwise error data as grouped boxplots
-x = np.array([1, 2, 3, 4, 11, 12, 13, 14, 21, 22, 23, 24, 31, 32, 33, 34, 41, 42, 43, 44])
-y = np.array(data).T
+groups = np.arange(0, (NUM_GROUPS + PAD - 1) * NUM_GROUPS).reshape((NUM_GROUPS, -1))[:, :-PAD]
+x = groups.flatten()
+y = np.array(data).T * MICRON_PER_PIXEL
 boxes = plt.boxplot(y, positions=x, patch_artist=True, medianprops=dict(color='black'))
 
 # color boxes and outlier points
@@ -37,8 +45,9 @@ for flier, color in zip(boxes['fliers'], colors):
     flier.set(marker='o', markerfacecolor=color)
 
 plt.ylabel('Pairwise error [µm]')
-plt.xlim(-2, 47)
-plt.xticks([2.5, 12.5, 22.5, 32.5, 42.5], ['STIM', 'Human #1', 'Human #2', 'Human #3', 'Human #4'])
+plt.xlim(np.min(groups) - PAD / 2, np.max(groups) + PAD / 2)
+plt.grid(axis='y', linestyle='--', alpha=0.5)
+plt.xticks(np.mean(groups, axis=1), LABELS, fontsize='small')
 xtick_labels = plt.gca().get_xticklabels()
 for label, color in zip(xtick_labels, COLORS):
     label.set_color(color)
@@ -46,6 +55,8 @@ for label, color in zip(xtick_labels, COLORS):
 plt.savefig('pairwise_error.pdf')
 # plt.show()
 
+
+# ---- Parameter scan ----
 
 # load parameter scan data
 file_name = os.path.join(script_dir, 'comparison-parameter-scan.csv')
@@ -62,20 +73,18 @@ for i in range(NUM_GROUPS):
     start_idx = i * GROUP_SIZE
     end_idx = start_idx + GROUP_SIZE
     x_values = np.arange(start_idx, end_idx) + i * SPACING
-    plt.plot(
-        x_values,
-        df['mean'][start_idx:end_idx],
-        marker='o',
-        label=f'Scale {df["scale"][start_idx]}'
-    )
+    y_values = df['mean'][start_idx:end_idx] * MICRON_PER_PIXEL
+    plt.plot(x_values, y_values, marker='o', label=f'Scale {df["scale"][start_idx]}')
 
 # set xticks to be only the 'renderFactor'
 xticks_labels = [f"{renderFactor}" for renderFactor in df['renderFactor']]
-plt.xticks(ticks=np.arange(len(df)) + (np.arange(len(df)) // GROUP_SIZE) * SPACING, labels=xticks_labels, rotation=45, ha='right')
+ticks = np.arange(len(df)) + (np.arange(len(df)) // GROUP_SIZE) * SPACING
+plt.xticks(ticks=ticks, labels=xticks_labels, rotation=45, ha='right')
 
 plt.xlabel('Render Factor')
 plt.ylabel('Distance to Human #1 [µm]')
-plt.legend()
+plt.legend(loc='lower left', fontsize='large', facecolor=(1, 1, 1, 1))
+plt.grid(linestyle='--', alpha=0.5)
 plt.tight_layout()
 plt.savefig('parameter_scan.pdf')
 # plt.show()
